c0135e36d4c9d6dd36b100e9bc0e076286f51bdf
repeat-tech <suspctng@pm.me>
2023-12-22 19:07:05 -0600

    Update wine-hill.handler.sh

--- wine-hill.handler.sh
+++ wine-hill.handler.sh
@@ -1,5 +1,7 @@
-if [ "$1" ]; then
-    WINEPREFIX=$HOME/.wine wine $HOME/.wine/drive_c/users/$USER/Application\ Data/Brick\ Hill/Player.exe $1
+if [[ "$1" == *"client"* ]]; then
+    client=$(echo "$1" | sed 's/client\///')
+    echo "$client"
+    WINEPREFIX=$HOME/.wine wine $HOME/.wine/drive_c/users/$USER/Application\ Data/Brick\ Hill/Player.exe "$client"
 else
     echo "no arguments, opening workshop..."
     WINEPREFIX=$HOME/.wine wine $HOME/.wine/drive_c/users/$USER/Application\ Data/Brick\ Hill/Workshop.exe
