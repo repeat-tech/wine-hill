19d86d2b45c567ef4951e4a493445c7f17b406c2
repeat-tech <suspctng@pm.me>
2023-12-25 04:46:58 -0600

    fixed function order

--- wine-hill.sh
+++ wine-hill.sh
@@ -3,36 +3,6 @@
 
 FILE="$HOME/.wine/drive_c/users/$USER/Application Data/Brick Hill/Player.exe"
 
-Uri () 
-{
-    if test -f "$FILE"; then
-        echo "brick-hill client is installed, installing URI..."
-    else
-        echo "brick-hill client is not installed, installing..."
-        wget https://brkcdn.com/downloads/BrickHillSetup.exe && WINEPREFIX=$HOME/.wine wine ./BrickHillSetup.exe
-    fi
-}
-
-if [[ "$1" == "--gamma"  ]] || [[ "$1" == "-g"  ]]; then
-    echo "installing gamma client..."
-    if test -f "$FILE"; then
-        Gammainstall() 
-        {
-        cd "$HOME/.wine/drive_c/users/$USER/Application Data/Brick Hill/"
-        sudo rm ./Player.exe
-        wget "https://github.com/jbfodevstyle/Gamma/raw/main/Player.exe" && echo "gamma client is installed!"
-        }
-        Gammainstall
-    else
-        Uri
-    fi
-else
-    echo "wine-hill v0.2 by repeat-tech"
-    echo "you should already have wine installed..."
-    sleep 2
-fi
-
-
 Handler ()
 {
     if test -f "./wine-hill.handler.sh"; then
@@ -60,5 +30,36 @@ Handler ()
     fi
     sleep 2
 }
+
+Uri () 
+{
+    if test -f "$FILE"; then
+        echo "brick-hill client is installed, installing URI..."
+    else
+        echo "brick-hill client is not installed, installing..."
+        wget https://brkcdn.com/downloads/BrickHillSetup.exe && WINEPREFIX=$HOME/.wine wine ./BrickHillSetup.exe
+        Handler
+    fi
+}
+
+if [[ "$1" == "--gamma"  ]] || [[ "$1" == "-g"  ]]; then
+    echo "installing gamma client..."
+    if test -f "$FILE"; then
+        Gammainstall() 
+        {
+        cd "$HOME/.wine/drive_c/users/$USER/Application Data/Brick Hill/"
+        sudo rm ./Player.exe
+        wget "https://github.com/jbfodevstyle/Gamma/raw/main/Player.exe" && echo "gamma client is installed!"
+        }
+        Gammainstall
+    else
+        Uri
+    fi
+else
+    echo "wine-hill v0.2 by repeat-tech"
+    echo "you should already have wine installed..."
+    sleep 2
+    Uri
+fi
 
 echo "you did it!"
